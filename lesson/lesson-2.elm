2c08b9a24fb3e5184b060999175df314bb550fc3
dhaneshkk <dhaneshkk@gmail.com>
2015-11-27 13:48:42 +0530

    adding startApp.simple  module

--- lesson/lesson-2.elm
+++ lesson/lesson-2.elm
@@ -1,16 +1,19 @@
+import StartApp.Simple exposing (start)
 import Graphics.Element exposing (..)
 import Mouse
+
 
 type alias Model = Int
 
 initialModel :Model
 initialModel = 0
 
+type Action = Increment
 
-
-update: a->Model->Model
-update event model
-        = model+1
+update : Action -> Model -> Model
+update action model
+        case action of 
+          Increment -> model+1
 
 view: Model->Element
 view model =
@@ -20,7 +23,10 @@ model: Signal Model
 model =
     Signal.foldp update initialModel Mouse.clicks
 
-main : Signal Element
 main =
-  Signal.map view model
+  start
+    { model = initialModel
+    , update = update
+    , view = view
+    }
 
